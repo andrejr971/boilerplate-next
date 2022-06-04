9fe0e4bece271df533c7605ba2f35a514aae27e4
André Junior <andre.souza64@fatec.sp.gov.br>
2022-06-04 11:12:43 -0300

    add styled-components

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/utils/*.ts',
+    '!src/hooks/*.tsx',
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
 };
---
+++ src/components/Main/test.spec.tsx
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/renderWithTheme';
+import { Main } from '.';
+
+describe('<Main />', () => {
+  it('should render heading', () => {
+    const { container } = renderWithTheme(<Main />);
+
+    expect(screen.getByRole('heading', { name: /test/i })).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
